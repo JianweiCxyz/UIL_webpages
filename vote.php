7edbac00a7e402dbfd436ddb98605def8de90276
Jianwei Chen <stevencjw13@gmail.com>
2018-03-25 10:54:08 -0500

    fix php errors

--- vote.php
+++ vote.php
@@ -1,16 +1,24 @@
 <?php
 $geoid = intval($_GET['geoid']);
-$vote = intval($_GET['vote'])
+$vote = intval($_GET['vote']);
 
-$con = mysqli_connect('mysql.utweb.utexas.edu','utw10792','NVM8z61FI9SIXFWa','utw10792');
+# $con = mysqli_connect('mysql.utweb.utexas.edu','utw10792','NVM8z61FI9SIXFWa','utw10792');
+$con = mysqli_connect('localhost','cjw','zyf','UILTest');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
 mysqli_select_db($con,"utw10792");
 $date = date_create();
-$timestamp = date_timestamp_get($date)
-$sql="INSERT INTO VoteLog VALUES ($geoid, $vote, $_SERVER['REMOTE_ADDR'], $timestamp);";
+$timestamp = date_timestamp_get($date);
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+$sql="INSERT INTO VoteLog VALUES ($geoid, $vote, \"$ip\", now());";
 $result = mysqli_query($con, $sql);
 
 if (mysqli_query($con, $sql)) {
