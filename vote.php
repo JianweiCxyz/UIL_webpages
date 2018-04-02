18813f57574dd32778ef8e96c5728c4857574880
Jianwei Chen <stevencjw13@gmail.com>
2018-04-01 22:32:33 -0500

    revised forms

--- vote.php
+++ vote.php
@@ -1,13 +1,20 @@
 <?php
-$geoid = intval($_GET['geoid']);
-$vote = intval($_GET['vote']);
+$geoid = intval($_POST['geoid']);
+$vote = intval($_POST['opinion']);
+$why = 0;
+if (array_key_exists('why', $_POST))
+    $why = intval($_POST['why']);
+if ($vote == 0)
+    $reason = $_POST['reasonNo'];
+else
+    $reason = $_POST['reasonYes'];
 
 $con = mysqli_connect('mysql.utweb.utexas.edu','utw10792','password','utw10792');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-mysqli_select_db($con,"utw10792");
+mysqli_select_db($con,"Votes");
 $date = date_create();
 $timestamp = date_timestamp_get($date);
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
@@ -17,12 +24,22 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 } else {
     $ip = $_SERVER['REMOTE_ADDR'];
 }
-$sql="INSERT INTO VoteLog VALUES ($geoid, $vote, \"$ip\", now()) ON DUPLICATE KEY UPDATE
-	vote='$vote', time=now();";
-$result = mysqli_query($con, $sql);
+$sql="INSERT INTO Votes VALUES ($geoid, $vote, $why, \"$reason\", now(), \"$ip\");";
 
 if (mysqli_query($con, $sql)) {
-    echo "OK";
+    $response = array(
+        'Thank you, You are a wonderful citizen.',
+        'Thank you, you are doing a great work for your city and community.',
+        'Thank you, you are like a NBA All Star to us.',
+        'Thank you for your great knowledge about local transit .',
+        'Great job, well done.',
+        'Thank you, your opinions will help our research greatly.',
+        'These are great answers, thank you.',
+        'Thank you for your Superbowl MVP level answers.',
+        'Thank you, you are our rock star.',
+        'Thank you, You are a wonderful human being. ',
+    );
+    echo $response[array_rand($response, 1)];
 }
 else {
 	echo "Error";
